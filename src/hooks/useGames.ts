458fa2e8a23dd3bbb03b97e67e8d9a89250e6a5d
Taslim Hossain <taslim.hossain.work@gmail.com>
2024-08-26 23:19:05 +0600

    Building Game Card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import axios, { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
   }
   
   interface FetchGamesResponse {

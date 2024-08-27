b022a8fd6a6b913d5b5d93ba1ece5013fb4b1018
Taslim Hossain <taslim.hossain.work@gmail.com>
2024-08-28 03:30:15 +0600

    Display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function GameCardContainer({ children }: Props) {
   return (
-    <Box width="323.5px" borderRadius={10} overflow="hidden">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
